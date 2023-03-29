9675093967d1fc270054ff9e918406b89a482ff0
ZeinShehab <zeinshehab@outlook.com>
2023-03-29 14:15:07 +0300

    Fixed bug causing invalid registration

--- client/src/helper/helper.js
+++ client/src/helper/helper.js
@@ -37,14 +37,9 @@ export async function getUser({ username }){
 /** register user function */
 export async function registerUser(credentials){
     try {
-        const { data : { msg }, status } = await axios.post(`/api/register`, credentials);
+        const { msg , status } = await axios.post(`/api/register`, credentials);
 
         let { username, email } = credentials;
-
-        /** send email */
-        if(status === 201){
-            await axios.post('/api/registerMail', { username, userEmail : email, text : msg})
-        }
 
         return Promise.resolve(msg)
     } catch (error) {
