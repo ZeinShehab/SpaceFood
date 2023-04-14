08f48d2cb30e19f7dfa73f9bed9376e5c96d2ec3
RaedFidawi <roudyfidawi@hotmail.com>
2023-04-15 01:29:08 +0300

    fixed issue with nav bar

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -70,10 +70,10 @@ export default function Post(props) {
         </div>
         <ul className="nav-links">
           <li>
-            <Link to="/Username.js" onClick={userLogout}>Logout</Link>
+            <Link to="/Username" onClick={userLogout}>Logout</Link>
           </li>
           <li>
-            <Link to="/profile.js">Profile</Link>
+            <Link to="/profile">Profile</Link>
           </li>
         </ul>
       </nav>
