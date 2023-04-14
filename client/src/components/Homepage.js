6d8433651cd8a3d1580a67f7d4a814a10d8d2c9f
Issa <i.itani03@hotmail.com>
2023-04-14 17:17:49 +0300

    added search bar

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,32 +1,39 @@
 import React, { useState } from 'react'
-import { Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import '../styles/Homepage.css'
 import { useEffect } from 'react'
 import useFetch from '../hooks/fetch.hook'
 import PostList from './PostList'
 import { getAllPosts } from '../helper/helper';
 import Post from './Post'
-export default function Homepage(){
-    const navigate = useNavigate()
-    const [posts,setPosts]= useState();
-    const [{apiData}] =useFetch()
-    function userLogout(){
-        localStorage.removeItem('token');
-        navigate('/')
+// import {getCategories} from '../redux/actions/categoryActions';
+export default function Homepage() {
+  const [text, setText] = useState('');
+  const navigate = useNavigate()
+  const [posts, setPosts] = useState();
+  const [{ apiData }] = useFetch()
+  function userLogout() {
+    localStorage.removeItem('token');
+    navigate('/')
+  }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const postsData = await getAllPosts();
+        setPosts(postsData.data);
+        console.log(postsData)
+      } catch (error) {
+        console.log(error);
       }
-      useEffect(() => {
-        async function fetchData() {
-          try {
-            const postsData = await getAllPosts();
-            setPosts(postsData.data);
-            console.log(postsData)
-          } catch (error) {
-            console.log(error);
-          }
-        }
-        fetchData();
-      }, []);
+    }
+    fetchData();
+  }, []);
 
+  // const handleSearch = e => {
+  //   setText(e.target.value);
+
+  //   dispatch(getProductsByFilter({type: 'text,', query: e.target.value}))
+  // }
   return (
     <div>
       <nav>
@@ -48,22 +55,35 @@ export default function Homepage(){
 
       <section className="recent-posts">
         <div className="post-section">
-        <h2 className="TempHeader">Recent Posts</h2>
-        
+          <h2 className="TempHeader">Recent Posts</h2>
+        </div>
+        <div className='search-bar'>
+          <div className='text-muted'>
+            {/* Filters <span className='fad fa-sliders-h'> </span> */}
+          </div>
+          <nav className='navbar navbar-expand-lg navbar-light bg-light border-top p-3'>
+            <form className="form-inline my-2 my-lg-0">
+              <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"  />
+              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+          </nav>
+          <div>
+
+          </div>
         </div>
         <div className="post-grid">
-          {posts? posts.map((post)=>(
+          {posts ? posts.map((post) => (
             // {const url = `/Post/${post.Id}`};
-          <Link to={`/post/${post._id}`} state={post._id}> 
-            <div className="post">
-              <img src={post.photo} alt="Post Image" />
-              <h3>{post.title}</h3>
-              <p>{
-                post.description.length >= 85 ? post.description.substring(0,80) + "..." :
-              post.description
-              }</p>
-            </div>
-          </Link>)):<h1>Loading</h1>}        
+            <Link to={`/post/${post._id}`} state={post._id}>
+              <div className="post">
+                <img src={post.photo} alt="Post Image" />
+                <h3>{post.title}</h3>
+                <p>{
+                  post.description.length >= 85 ? post.description.substring(0, 80) + "..." :
+                    post.description
+                }</p>
+              </div>
+            </Link>)) : <h1>Loading</h1>}
         </div>
       </section>
     </div>
