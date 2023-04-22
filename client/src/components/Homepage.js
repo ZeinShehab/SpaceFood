0000de40ffdee517ce3fdff1a4b6ea3ce9f5cb3d
Issa <i.itani03@hotmail.com>
2023-04-22 17:32:21 +0300

    fixed front-end js files

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,71 +1,71 @@
 import React, { useState } from 'react'
-import { Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import '../styles/Homepage.css'
 import { useEffect } from 'react'
 import useFetch from '../hooks/fetch.hook'
 import PostList from './PostList'
 import { getAllPosts } from '../helper/helper';
 import Post from './Post'
-export default function Homepage(){
-    const navigate = useNavigate()
-    const [posts,setPosts]= useState();
-    const [{apiData}] =useFetch()
-    function userLogout(){
-        localStorage.removeItem('token');
-        navigate('/')
+export default function Homepage() {
+  const navigate = useNavigate()
+  const [posts, setPosts] = useState();
+  const [{ apiData }] = useFetch()
+  function userLogout() {
+    localStorage.removeItem('token');
+    navigate('/')
+  }
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const postsData = await getAllPosts();
+        setPosts(postsData.data);
+        console.log(postsData)
+      } catch (error) {
+        console.log(error);
       }
-      useEffect(() => {
-        async function fetchData() {
-          try {
-            const postsData = await getAllPosts();
-            setPosts(postsData.data);
-            console.log(postsData)
-          } catch (error) {
-            console.log(error);
-          }
-        }
-        fetchData();
-      }, []);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
-      <nav>
+      <nav className='homepage-header'>
         <div className="logo">
           <Link to="/Homepage">SpaceFood</Link>
         </div>
-        <ul className="nav-links">
-          <li>
-            <Link to="/" onClick={userLogout}>Logout</Link>
-          </li>
+        <div className="nav-links">
           <li>
             <Link to="/profile">Profile</Link>
           </li>
           <li>
             <Link to="/Post" >Post a recipe</Link>
           </li>
-        </ul>
+          <li>
+            <Link to="/" onClick={userLogout}>Logout</Link>
+          </li>
+        </div>
       </nav>
 
-      <section className="recent-posts">
+      <div className="recent-posts">
         <div className="post-section">
-        <h2 className="TempHeader">Recent Posts</h2>
-        
+          <div className="TempHeader">Recent Posts</div>
         </div>
+
         <div className="post-grid">
-          {posts? posts.map((post)=>(
+          {posts ? posts.map((post) => (
             // {const url = `/Post/${post.Id}`};
-          <Link to={`/post/${post._id}`} state={post._id}> 
-            <div className="post">
-              <img src={post.photo} alt="Post Image" />
-              <h3>{post.title}</h3>
-              <p>{
-                post.description.length >= 85 ? post.description.substring(0,80) + "..." :
-              post.description
-              }</p>
-            </div>
-          </Link>)):<h1>Loading</h1>}        
+            <Link className='card-link' to={`/post/${post._id}`} state={post._id}>
+              <div className="post">
+                <div className='card-title'>{post.title}</div>
+                <img src={post.photo} alt="Post Image" />
+                <div className='card-description'>{
+                  post.description.length >= 85 ? post.description.substring(0, 80) + "..." :
+                    post.description
+                }</div>
+              </div>
+            </Link>)) : <h1>Loading</h1>}
         </div>
-      </section>
+      </div>
     </div>
   )
 }
--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { getAllPosts } from '../helper/helper';
 import '../styles/PostList.css'
-import{Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 function PostList() {
-    const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState([]);
   useEffect(() => {
     async function fetchData() {
       try {
@@ -18,14 +18,14 @@ function PostList() {
   }, []);
   return (
     <div className='post-list'>
-      {posts? posts.map((post) => (
+      {posts ? posts.map((post) => (
         <div className="post">
-          <img src={post.photo}/>
+          <img src={post.photo} />
           <h2>{post.title}</h2>
-          <p>posted by: {post.owner}</p>
-          <p>Description: {post.description && post.description}</p>
+          <div>posted by: {post.owner}</div>
+          <div>Description: {post.description && post.description}</div>
         </div>
-      )):<h1>Loading Posts</h1>}
+      )) : <h1>Loading Posts</h1>}
     </div>
 
   );
