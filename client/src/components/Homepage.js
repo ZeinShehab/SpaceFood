91e11be57bfe9c3507fcb4909625f1d6ae57cd6e
RaedFidawi <roudyfidawi@hotmail.com>
2023-04-22 17:58:55 +0300

    user can search posts by title or by tag

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,31 +1,48 @@
-import React, { useState } from 'react'
-import { Link, useNavigate} from 'react-router-dom'
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import '../styles/Homepage.css'
-import { useEffect } from 'react'
 import useFetch from '../hooks/fetch.hook'
 import PostList from './PostList'
 import { getAllPosts } from '../helper/helper';
 import Post from './Post'
-export default function Homepage(){
-    const navigate = useNavigate()
-    const [posts,setPosts]= useState();
-    const [{apiData}] =useFetch()
-    function userLogout(){
-        localStorage.removeItem('token');
-        navigate('/')
+
+export default function Homepage() {
+  const navigate = useNavigate()
+  const [posts, setPosts] = useState(null)
+  const [{ apiData }] = useFetch()
+  const [search, setSearch] = useState('')
+  const [tag, setTag] = useState('')
+
+  function userLogout() {
+    localStorage.removeItem('token')
+    navigate('/')
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const postsData = await getAllPosts()
+        setPosts(postsData.data)
+      } catch (error) {
+        console.log(error)
       }
-      useEffect(() => {
-        async function fetchData() {
-          try {
-            const postsData = await getAllPosts();
-            setPosts(postsData.data);
-            console.log(postsData)
-          } catch (error) {
-            console.log(error);
-          }
-        }
-        fetchData();
-      }, []);
+    }
+    fetchData()
+  }, [])
+
+  let filteredPosts = posts
+
+  if (search.trim() !== '' && posts) {
+    filteredPosts = filteredPosts.filter(post =>
+      post.title.toLowerCase().includes(search.toLowerCase())
+    )
+  }
+  
+  if (tag.trim() !== '' && posts) {
+    filteredPosts = filteredPosts.filter(post =>
+      post.tags.some(postTag => postTag.toLowerCase().includes(tag.toLowerCase()))
+    )
+  }
 
   return (
     <div>
@@ -47,23 +64,35 @@ export default function Homepage(){
       </nav>
 
       <section className="recent-posts">
+          <h2 className="TempHeader">Recent Posts</h2>
         <div className="post-section">
-        <h2 className="TempHeader">Recent Posts</h2>
-        
+          <form className="search-form">
+            <input
+              className="searchBar"
+              type="text"
+              placeholder="Search by title"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <input
+              className="searchBar"
+              type="text"
+              placeholder="Search by tag"
+              value={tag}
+              onChange={(e) => setTag(e.target.value)}
+            />
+          </form>
         </div>
         <div className="post-grid">
-          {posts? posts.map((post)=>(
-            // {const url = `/Post/${post.Id}`};
-          <Link to={`/post/${post._id}`} state={post._id}> 
-            <div className="post">
-              <img src={post.photo} alt="Post Image" />
-              <h3>{post.title}</h3>
-              <p>{
-                post.description.length >= 85 ? post.description.substring(0,80) + "..." :
-              post.description
-              }</p>
-            </div>
-          </Link>)):<h1>Loading</h1>}        
+          {filteredPosts ? filteredPosts.map(post => (
+            <Link to={`/post/${post._id}`} state={post._id}>
+              <div className="post" key={post._id}>
+                <img src={post.photo} alt="Post Image" />
+                <h3>{post.title}</h3>
+                <p>{post.description.length >= 85 ? `${post.description.substring(0, 80)}...` : post.description}</p>
+              </div>
+            </Link>
+          )) : <h1>Loading</h1>}
         </div>
       </section>
     </div>
