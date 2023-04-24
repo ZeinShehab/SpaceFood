a672f61fa20165082720a5e0ca0e1eafb5fcbffe
Issa <i.itani03@hotmail.com>
2023-04-24 22:30:20 +0300

    created viewposts

---
+++ client/src/components/ViewPosts.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import useFetch from "../hooks/fetch.hook";
+import axios from "axios";
+import { Link, useNavigate } from 'react-router-dom'
+
+export default function ViewBookmarks() {
+    const navigate = useNavigate()
+    function userLogout() {
+        localStorage.removeItem('token')
+        navigate('/')
+      }
+    return (
+        <nav className='homepage-header'>
+        <div className="logo">
+          <Link to="/Homepage">SpaceFood</Link>
+        </div>
+        <div className="nav-links">
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link to="/Post" >Post a recipe</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={userLogout}>Logout</Link>
+          </li>
+          <li>
+            <Link to ="/Bookmarks">Bookmarks</Link>
+          </li>
+        </div>
+        <h1>hi</h1>
+      </nav>
+        // <div className="post-grid">
+        //     {bookmarks ? bookmarks.map(post => (
+        //         <Link to={`/post/${post._id}`} state={post._id}>
+        //             <div className="post" key={post._id}>
+        //                 <img src={post.photo} alt="Post Image" />
+        //                 <h3>{post.title}</h3>
+        //                 <p>{post.description.length >= 85 ? `${post.description.substring(0, 80)}...` : post.description}</p>
+        //             </div>
+        //         </Link>
+        //     )) : <h1>Loading</h1>}
+        // </div>
+        
+    );
+}
