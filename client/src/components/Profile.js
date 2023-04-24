b916caec665342339c77f9591a0baa93c086f3d1
Issa <i.itani03@hotmail.com>
2023-04-24 15:14:51 +0300

    fixed profile form

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -101,7 +101,7 @@ export default function Profile() {
       <Toaster position='top-center' reverseOrder={false}></Toaster>
 
       <div className='flex justify-center items-center h-screen'>
-        <div className={`${styles.glass} ${extend.glass}`} style={{ width: "45%", paddingTop: '0em'}}>
+        <div className={`${styles.glass} ${extend.glass}`} style={{ width: "45%", paddingTop: '3em'}}>
 
           <div className="title flex flex-col items-center">
             <div className="flex gap-40">
